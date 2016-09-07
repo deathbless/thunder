78a36cd9a75b5ef91dc05d4b3931a323b0a76832
hzsunyuda <deathbless@163.com>
2016-09-07 16:06:46 +0800

    玄学加速完成,起码看起来显示加速了

--- main.py
+++ main.py
@@ -2,6 +2,27 @@
 __author__ = 'deathbless'
 
 import os
+import sqlobject
+from sqlobject.sqlbuilder import *
+from connection import conn
+
+class data(sqlobject.SQLObject):
+    _connection = conn
+
+
+def find():
+    tables = conn.listTables()
+    for table in tables:
+        if "superspeed" in table or "offline" in table:
+            print table
+            read = conn.queryAll("SELECT UserData FROM %s" % table)
+            string = str(read[0][0]).decode("utf-8")
+            string = eval(string)
+            string['Message'] = "fuck u thunder"
+            string['Result'] = 0
+            update = Update(table, values={"UserData":str(string).replace("'", '"').encode("utf-8")})
+            query = conn.sqlrepr(update)
+            conn.query(query)
 
 if __name__ == '__main__':
-    pass
+    find()
