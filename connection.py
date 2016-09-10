4087c43d595699e40f55821c6d16d253747caa14
deathbless <deathbless@163.com>
2016-09-11 00:41:02 +0800

    多任务添加支持，但是傻逼sqlobject在插入的时候会多加空格，不知道怎么解决

--- connection.py
+++ connection.py
@@ -4,4 +4,5 @@ __author__ = 'deathbless'
 from sqlobject.sqlite import builder
 # name = raw_input()
 name = "D:/Program Files (x86)/Thunder Network/Thunder9/Profiles/TaskDb.dat"
+# name = "TaskDb.dat"
 conn = builder()(name)
--- main.py
+++ main.py
@@ -5,26 +5,66 @@ import os
 import sqlobject
 from sqlobject.sqlbuilder import *
 from connection import conn
+import array
+
 
 class data(sqlobject.SQLObject):
     _connection = conn
 
 num = 0
+tasks = []  # 已经加速的任务，可能有重复因此用list记录下
+allTasks = []  # 总共的任务，显示数量用
 flyNum = 0
 
 #TODO 多任务支持,一个任务可能有两个TaskId
 def getTask():
+    global num, allTasks
     tables = conn.listTables()
     for table in tables:
         if "superspeed" in table or "offline" in table:
-            read = conn.queryAll("SELECT LocalTaskId FROM %s" % table)
-            print read
-            read = conn.queryAll("SELECT UserData FROM %s" % table)
-            for one in read:
-                print str(one[0])
-            exit(0)
+            read = conn.queryAll("SELECT LocalTaskId,AccelerateTaskId,LocalSubFileIndex FROM %s" % table)
+            for task in read:
+                crack(task[0], table, task[1], task[2])
+                if task[0] not in allTasks:
+                    num += 1
+                    allTasks.append(task[0])
+
+def crack(taskId, tableName, AccTaskId, LocalSub):
+    global flyNum, tasks
+    data = conn.queryAll("SELECT UserData FROM %s WHERE LocalTaskId=%s AND AccelerateTaskId=%s AND LocalSubFileIndex=%s"\
+                         % (tableName, taskId, AccTaskId, LocalSub))  # 获取到单个任务的数值
+
+    # data获取到的是一个集合，虽然这个集合里面只有一个任务（where条件保证）
+    UserData = data[0][0]
+    string = str(UserData)
+    string = eval(string)
+    if "Message" in string.keys():
+        string['Message'] = "fuck u thunder"
+    if "Result" in string.keys():
+        if string['Result'] == 0:
+            return
+        string['Result'] = 0
+    update = Update(tableName, values={"UserData": buffer(array.array('c',str(string).replace("'", '"')))},\
+                    where="LocalTaskId=%s AND AccelerateTaskId=%s AND LocalSubFileIndex=%s"\
+                          % (taskId, AccTaskId, LocalSub))
+    query = conn.sqlrepr(update)
+    # print query
+    conn.query(query)
+    # 检测是否成功改变
+
+    data = conn.queryAll("SELECT UserData FROM %s WHERE LocalTaskId=%s AND AccelerateTaskId=%s AND LocalSubFileIndex=%s"\
+                         % (tableName, taskId, AccTaskId, LocalSub))  # 获取到单个任务的数值
+    UserData = data[0][0]
+    string = str(UserData).decode("utf-8")
+    string = eval(string)
+    if "Result" in string.keys():
+        if string['Result'] == 0 and taskId not in tasks:
+            flyNum += 1
+            tasks.append(taskId)
+
 
 def find():
+    # 测试用函数，已舍弃
     global num
     tables = conn.listTables()
     for table in tables:
@@ -44,8 +84,10 @@ def find():
             read = conn.queryAll("SELECT UserData FROM %s" % table)
             string = str(read[0][0]).decode("utf-8")
             string = eval(string)
-            if string.get('Result',-1) == 0:
+            if string.get('Result', -1) == 0:
                 num += 1
 
 if __name__ == '__main__':
+    print "现在正在读取数据库..."
     getTask()
+    print "执行完毕，现在总共有%s个任务在下载中,新加速了%s个任务" % (num, flyNum)
